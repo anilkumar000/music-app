4edfffd619b858f5ac40237002eb15c9758d868c
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-08-09 14:53:08 +0530

    created dockerfile for each service and compose.yml created for complete app

--- api-gateway/src/main/java/com/stackroute/apigatewaypattern/ApiGatewayPatternApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigatewaypattern/ApiGatewayPatternApplication.java
@@ -2,9 +2,11 @@ package com.stackroute.apigatewaypattern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 //Annotation for enabling proxy for the services
+@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayPatternApplication {
